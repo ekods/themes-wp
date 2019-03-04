d3ef124a506ec0a88fdf7e827edb6f0ae1a21211
ekods <ekodwisaputro.dev@gmail.com>
2019-03-05 06:26:33 +0700

    add meta and dns-prefetch google-analytics

--- header.php
+++ header.php
@@ -1,34 +1,38 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <?php $title = wp_title("", false);?>
-	<title><?php echo ($title == '') ? get_bloginfo('name') : $title .' - '. get_bloginfo('name'); ?></title>
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>">
 
-  <meta name="description" content="<?php if ( is_single() ) {
-          single_post_title('', true);
-      } else {
-          bloginfo('name'); echo " - "; bloginfo('description');
-      }
-      ?>" />
-  <meta name="keywords"  content="" />
-  <meta name="theme-color" content="#000">
+		<link href="//www.google-analytics.com" rel="dns-prefetch">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/images/favicon-72x72.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/images/favicon-114x114.png">
-  <link rel="apple-touch-icon" sizes="228x228" href="<?php echo get_template_directory_uri(); ?>/images/favicon-228x228.png">
-  <link rel="shortcut icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.png">
+    <?php $title = wp_title("", false);?>
+  	<title><?php echo ($title == '') ? get_bloginfo('name') : $title .' - '. get_bloginfo('name'); ?></title>
 
-  <link href="https://fonts.googleapis.com/css?family=Barlow:400,400i,500,500i,600,600i,700,700i,800,800i" rel="stylesheet">
+    <meta name="description" content="<?php if ( is_single() ) {
+            single_post_title('', true);
+        } else {
+            bloginfo('name'); echo " - "; bloginfo('description');
+        }
+        ?>" />
+    <meta name="keywords"  content="" />
+    <meta name="theme-color" content="#000">
 
-  <?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<?php endif; ?>
-	<?php wp_head(); ?>
+    <link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/images/favicon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/images/favicon-114x114.png">
+    <link rel="apple-touch-icon" sizes="228x228" href="<?php echo get_template_directory_uri(); ?>/images/favicon-228x228.png">
+    <link rel="shortcut icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.png">
 
-</head>
+    <link href="https://fonts.googleapis.com/css?family=Barlow:400,400i,500,500i,600,600i,700,700i,800,800i" rel="stylesheet">
+
+    <?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
+  	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+  	<?php endif; ?>
+  	<?php wp_head(); ?>
+
+  </head>
 
 <body <?php echo body_class();?>>
 
